58a7ad94e54f278552b0104f6c915bf3dbbfa803
Sumeet Fefar <sumeet.fefar@gmail.com>
2014-03-07 20:42:10 +0530

    Parsing the Graph Complete

--- main.cpp
+++ main.cpp
@@ -2,32 +2,39 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+#include <vector>
+
 
 using namespace std;
 
 
 class Node
 {
-int index;
-int card; 
+public:
+	int index;
+	int cardinality; 
+	bool * neighbours;
 };
 
 
 class Graph
 {
-/*
-protected:
-    int num_nodes;
-    int num_edges;
-*/
+
+
+private:
+    int num_nodes ;
+    int num_edges ;
+    Node * NodeArray;
+
 public:
-    int num_nodes;
-    int num_edges;
-    
+
     void parse(string fileName)
     {
         ifstream inf(fileName.c_str());
-        int line_no = 0;
+        int line_no = 1;
+        vector<string> strs;
 
         if (!inf)
         {
@@ -39,18 +46,48 @@ public:
         {
             std::string strInput;
             getline(inf, strInput);
-            if (line_no == 0)
+            if (line_no == 1)	
             {
-                num_nodes = atoi(strInput.c_str());
+            	num_nodes = atoi(strInput.c_str());
+            	NodeArray = new Node[num_nodes];
+            	for (int i=0;i<num_nodes;i++){
+            		NodeArray[i].neighbours = new bool[num_nodes] {false};
+            	}
+            }
+            else if (line_no == 2)	
+            {
+            	num_edges = atoi(strInput.c_str());
+           	}
+            else if (line_no <= num_nodes+2)
+            {
+            	boost::split(strs, strInput, boost::is_any_of(" "));
+            	NodeArray[atoi(strs.at(0).c_str())].cardinality = atoi(strs.at(1).c_str());
+            }
+            else if (line_no <= num_nodes + num_edges + 2)
+            {
+            	boost::split(strs, strInput, boost::is_any_of(" "));
+            	NodeArray[atoi(strs.at(0).c_str())].neighbours[atoi(strs.at(1).c_str())] = 1;
+            	NodeArray[atoi(strs.at(1).c_str())].neighbours[atoi(strs.at(0).c_str())] = 1;
             }
 
-            else if (line_no == 1)
-            {
-                num_edges = atoi(strInput.c_str());
-            }
             cout << strInput << endl;
             line_no++;
         }
+    }
+
+    void printParameters()
+    {
+    	cout << "Num of nodes : " << num_nodes << endl;
+    	cout << "Num of edges : " << num_edges << endl;
+    	for (int i = 0; i < num_nodes; i++)
+    	{
+    		cout<< "Node : "<<i<<" " << NodeArray[i].cardinality<<"   -> ";
+    		for (int j = 0; j < num_nodes; j++)
+    		{
+    			cout<< NodeArray[i].neighbours[j] << " ";
+    		}
+    		cout<<endl;
+    	}
     }
 };
 
@@ -58,8 +95,7 @@ int main()
 {
     Graph myGraph;
     myGraph.parse("samplegraph.txt");
-    cout << "Num of nodes : " << myGraph.num_nodes << endl;
-    cout << "Num of edges : " << myGraph.num_edges << endl;
+	myGraph.printParameters();
 
     return 0;
 }
